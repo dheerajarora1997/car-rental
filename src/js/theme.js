11420b00b464fb616f1f49d5c2c061442a89f63b
dheerajarora1997 <dheeraj.arora1997@yahoo.com>
2022-01-12 21:15:21 +0530

    initial setup

---
+++ src/js/theme.js
@@ -0,0 +1,11 @@
+if (jQuery('[data-toggle="tooltip"]').length) {
+    jQuery('[data-toggle="tooltip"]').tooltip();
+}
+
+if (jQuery('.selectpicker').length) {
+    jQuery('.selectpicker').selectpicker();
+}
+
+if (jQuery('.owl-carousel').length) {
+    jQuery('.owl-carousel').owlCarousel();
+}
